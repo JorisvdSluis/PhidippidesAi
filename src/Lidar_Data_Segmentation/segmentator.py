0ca0f3bef7cc834a96c38d51b4f6e2fca5364a90
jjori <jjori8@hotmail.com>
2020-10-28 13:56:10 +0100

    change height

--- src/Lidar_Data_Segmentation/segmentator.py
+++ src/Lidar_Data_Segmentation/segmentator.py
@@ -27,7 +27,7 @@ class segmentator:
 from read_json import read_json
 import json as json
 pointcloud = read_json()
-seg = segmentator(11.1111111, 4,-1,1.5)
+seg = segmentator(11.1111111, 4,-0.9,1.5)
 points = seg.segmentate(pointcloud)
 
 file = open("segmentation.txt", "a")
