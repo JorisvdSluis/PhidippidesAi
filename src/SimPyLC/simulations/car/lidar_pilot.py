547448228fd2bd43bacb517326010b255c76e89f
jjori <jjori8@hotmail.com>
2020-10-29 14:55:18 +0100

    added methods for readability

--- src/SimPyLC/simulations/car/lidar_pilot.py
+++ src/SimPyLC/simulations/car/lidar_pilot.py
@@ -69,13 +69,57 @@ class LidarPilot:
         for lidarAngle in range (-self.lidarHalfApertureAngle, self.lidarHalfApertureAngle):
             lidarDistance = self.lidarDistances [lidarAngle]
             obstacleDistance = self.obstacleDistances [lidarAngle]
-            
+            self.alternativeCoordinates = [0,0]
+
             #check if encountering obstacles
             self.avoidObstacleCollision(lidarAngle, obstacleDistance)
 
             # Detect 4 closest points of road borders
-            # discard points within 25 degrees to avoid a head on collision  
-            if lidarDistance < self.leftRoadBorder and lidarAngle < -25:
+            self.calculateFourClosestPoints()
+
+        #calculate coordinates for 4 closest points 
+        self.coordinatesNR = self.calculateCoordinates(self.nextRightRoadBorderAngle, self.nextRightRoadborder)
+        self.coordinatesR = self.calculateCoordinates(self.rightRoadBorderAngle, self.rightRoadborder)
+        self.coordinatesNL = self.calculateCoordinates(self.nextLeftRoadBorderAngle, self.nextLeftRoadBorder)
+        self.coordinatesL = self.calculateCoordinates(self.leftRoadBorderAngle, self.leftRoadBorder)
+        self.coordinatesO = self.calculateCoordinates(lidarAngle, obstacleDistance)
+        
+        #calculate coordinates for middle point with or without obstacle
+        if(obstacleDistance != self.leftRoadBorder and obstacleDistance != self.rightRoadborder ):
+            self.xMiddle = self.coordinatesL[0] + self.coordinatesNL[0] + self.coordinatesR[0] + self.coordinatesNR[0]
+            self.yMiddle = self.coordinatesL[1] + self.coordinatesNL[1] + self.coordinatesR[1] + self.coordinatesNR[1]
+        else:
+            self.xMiddle = (self.coordinatesO[0] + self.alternativeCoordinates[0])/2
+            self.yMiddle = (self.coordinatesO[1] + self.alternativeCoordinates[1])/2
+
+        #set steering angle
+        self.steeringAngle = sp.world.physics.steeringAngle
+        self.controlSteeringAngle()
+        #set velocity
+        self.targetVelocity = (90 - sp.abs (self.steeringAngle))/80 if self.driveEnabled else 0
+    
+    def avoidObstacleCollision(self, lidarAngle, obstacleDistance):
+        if(lidarAngle > -25 and lidarAngle < 25):
+            #check if obstacle closer than road borders
+            if((obstacleDistance < self.leftRoadBorder and self.leftRoadBorder < sp.finity) or  (obstacleDistance < self.rightRoadborder and self.rightRoadborder < sp.finity) ): 
+                #find biggest gap to drive through 
+                if(sp.abs(obstacleDistance - self.leftRoadBorder) > sp.abs(obstacleDistance - self.rightRoadborder) ):
+                    self.rightRoadborder = obstacleDistance
+                    self.rightRoadBorderAngle = lidarAngle
+                else:
+                    self.leftRoadBorder = obstacleDistance
+                    self.leftObstacleAngle = lidarAngle
+            elif((obstacleDistance < self.nextLeftRoadBorder and self.nextLeftRoadBorder < sp.finity) or  (obstacleDistance < self.nextRightRoadborder and self.nextRightRoadborder < sp.finity)):
+                if(sp.abs(obstacleDistance - self.nextLeftRoadBorder) > sp.abs(obstacleDistance - self.nextRightRoadborder) ):
+                    self.nextRightRoadborder = obstacleDistance
+                    self.nextRightRoadBorderAngle = lidarAngle
+                else:
+                    self.nextLeftRoadBorder = obstacleDistance
+                    self.nextLeftRoadBorderAngle = lidarAngle
+
+    def calculateFourClosestPoints(self):
+        # discard points within 25 degrees to avoid a head on collision  
+        if lidarDistance < self.leftRoadBorder and lidarAngle < -25:
                 self.nextLeftRoadBorder =  self.leftRoadBorder
                 self.nextLeftRoadBorderAngle = self.leftRoadBorderAngle 
                 self.leftRoadBorder = lidarDistance 
@@ -92,46 +136,29 @@ class LidarPilot:
                 self.nextRightRoadborder = lidarDistance
                 self.nextRightRoadBorderAngle = lidarAngle
 
-        #calculate coordinates for 4 closest points 
-        self.coordinatesNR = self.calculateCoordinates(self.nextRightRoadBorderAngle, self.nextRightRoadborder)
-        self.coordinatesR = self.calculateCoordinates(self.rightRoadBorderAngle, self.rightRoadborder)
-        self.coordinatesNL = self.calculateCoordinates(self.nextLeftRoadBorderAngle, self.nextLeftRoadBorder)
-        self.coordinatesL = self.calculateCoordinates(self.leftRoadBorderAngle, self.leftRoadBorder)
-
-        #calculate coordinates for middle point  
-        self.xMiddle = self.coordinatesL[0] + self.coordinatesNL[0] + self.coordinatesR[0] + self.coordinatesNR[0]
-        self.yMiddle = self.coordinatesL[1] + self.coordinatesNL[1] + self.coordinatesR[1] + self.coordinatesNR[1]
-
-        #calculate angle and velocity from middle point
-        if(math.degrees(math.atan((self.yMiddle / self.xMiddle))) > 5 or math.degrees(math.atan((self.yMiddle / self.xMiddle))) < -5):
-            self.steeringAngle = (math.degrees(math.atan((self.yMiddle / self.xMiddle))) + (sp.world.physics.steeringAngle*15))/16
-        else:
-            self.steeringAngle = math.degrees(math.atan((self.yMiddle / self.xMiddle)))
-        self.targetVelocity = (90 - sp.abs (self.steeringAngle))/80 if self.driveEnabled else 0
-    
-    def avoidObstacleCollision(self, lidarAngle, obstacleDistance):
-        if(lidarAngle > -25 and lidarAngle < 25):
-            #check if obstacle closer than road borders
-            if((obstacleDistance < self.leftRoadBorder and self.leftRoadBorder < sp.finity) or  (obstacleDistance < self.rightRoadborder and self.rightRoadborder < sp.finity) ): 
-                #find biggest gap to drive through
-                if(sp.abs(obstacleDistance - self.leftRoadBorder) > sp.abs(obstacleDistance - self.rightRoadborder) ):
-                    self.rightRoadborder = obstacleDistance
-                    self.rightRoadBorderAngle = lidarAngle
-                else:
-                    self.leftRoadBorder = obstacleDistance
-                    self.leftObstacleAngle = lidarAngle
-            elif((obstacleDistance < self.nextLeftRoadBorder and self.nextLeftRoadBorder < sp.finity) or  (obstacleDistance < self.nextRightRoadborder and self.nextRightRoadborder < sp.finity)):
-                if(sp.abs(obstacleDistance - self.nextLeftRoadBorder) > sp.abs(obstacleDistance - self.nextRightRoadborder) ):
-                    self.nextRightRoadborder = obstacleDistance
-                    self.nextRightRoadBorderAngle = lidarAngle
-                else:
-                    self.nextLeftRoadBorder = obstacleDistance
-                    self.nextLeftRoadBorderAngle = lidarAngle
-
     def calculateCoordinates(self, angle, distance):
         x = math.cos(math.radians(angle)) * distance
         y = math.sin(math.radians(angle)) * distance
         return (x,y)
+
+    def controlSteeringAngle(self): 
+        soughtAfterAngle = math.degrees(math.atan((self.yMiddle / self.xMiddle)))
+        currentAngle = sp.world.physics.steeringAngle
+        diffrence = soughtAfterAngle - currentAngle
+        if(diffrence > 25 or diffrence < -25):
+            self.steeringAngle = currentAngle + (diffrence / 16)
+        elif(diffrence > 20 or diffrence < -20):
+            self.steeringAngle = currentAngle + (diffrence / 10)
+        elif(diffrence > 15 or diffrence < -15):
+            self.steeringAngle = currentAngle + (diffrence / 8)
+        elif(diffrence > 10 or diffrence < -10):
+            self.steeringAngle = currentAngle + (diffrence / 6)
+        elif(diffrence > 5 or diffrence < -5):
+            self.steeringAngle = currentAngle + (diffrence / 4)
+        elif(diffrence > 2.5 or diffrence < -2.5):
+            self.steeringAngle = currentAngle + (diffrence / 2)
+        else:
+             self.steeringAngle = currentAngle + diffrence
 
     def output (self):  # Output to simulator
         sp.world.physics.steeringAngle.set (self.steeringAngle)
